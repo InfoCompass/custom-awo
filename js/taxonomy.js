38e1398a19f1a914372da657fc7ef2bb7a984988
rhotep <andreas.pittrich@posteo.de>
2021-06-30 22:29:19 +0200

    removed duplicates from item edit

--- js/taxonomy.js
+++ js/taxonomy.js
@@ -117,7 +117,7 @@
 						'support_visiting_service',		// Besuchsdienste
 						'support_repairs',				// Reparaturhilfe
 						'support_helpline', 			// Telefonseelsorge
-						'support_public_restrooms'		// Öffentliche Toiletten
+						'support_public_restrooms',		// Öffentliche Toiletten
 						'support_mobility'				// Mobilitätshilfedienste
 					]
 		},
